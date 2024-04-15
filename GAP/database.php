d3753f14028f1ee26f6ff488aa2d03e4e6ba89ee
walidmadad <walid.madad123@gmail.com>
2024-04-15 20:01:45 +0200

    commit

--- GAP/database.php
+++ GAP/database.php
@@ -2,7 +2,7 @@
 
 function connectToDatabase()
 {
-    $conn = mysqli_connect("localhost", "root", "", "la_mise_au_vert");
+    $conn = mysqli_connect("localhost", "prof1234", "prof_1234!", "la_mise_au_vert");
     if ($conn->connect_error) {
         die("La connexion à la base de données a échoué : " . $conn->connect_error);
     }
